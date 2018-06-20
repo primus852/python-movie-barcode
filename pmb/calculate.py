9f9715769878996443a7b4ee87f2ae477c66ccea
PrimuS <findnibbler@gmail.com>
2018-06-20 12:32:54 +0200

    Wrong file path to save results

--- pmb/calculate.py
+++ pmb/calculate.py
@@ -27,6 +27,7 @@ def process_images(file, title, subtitle, width=1920, height=1080, folder='video
 
     # Get the relative path for output
     result_folder = path.join(path.dirname(__file__), output_folder)
+    print(result_folder)
 
     # Full path for video
     full_path = path.join(videos, file)
